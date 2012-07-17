306df9e3006ffb1770abe7792ecb88fa3e55bc4e
Oleg Bovykin mbpr <oleg.bovykin@gmail.com>
2012-07-17 15:08:27 +0400

    fix

--- lib/generators/redde/layout/layout_generator.rb
+++ lib/generators/redde/layout/layout_generator.rb
@@ -6,12 +6,11 @@ module Redde
       source_root File.expand_path("../templates", __FILE__)
       desc "Standart redde admin generator"
 
-      attr_reader :app_name, :container_class
+      attr_reader :app_name
 
       def generate_layout
         app = ::Rails.application
         @app_name = app.class.to_s.split("::").first
-        @container_class = layout_type == "fluid" ? "container-fluid" : "container"
         template "admin.html.haml", "app/views/layouts/admin.html.haml"
         template "login.html.haml", "app/views/layouts/login.html.haml"
       end
