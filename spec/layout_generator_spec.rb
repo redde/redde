27b8e03c5ea8bd7a7a4f8ad18e99e9a345fd5039
Oleg Bovykin mbpr <oleg.bovykin@gmail.com>
2012-07-18 16:18:55 +0400

    added spec for images

--- spec/layout_generator_spec.rb
+++ spec/layout_generator_spec.rb
@@ -17,13 +17,21 @@ describe Redde::Generators::LayoutGenerator do
   describe "layout" do
 
     it "creates a test initializer" do
+      # check layouts
       assert_file "app/views/layouts/admin.html.haml"
       assert_file "app/views/layouts/login.html.haml"
+
+      # check shared
       assert_file "app/views/admin/shared/_launchbar.html.haml"
       assert_file "app/views/admin/shared/_sidebar.html.haml"
       assert_file "app/views/admin/shared/_header.html.haml"
+
+      # check js and css
       assert_file "app/assets/javascripts/admin.js"
       assert_file "app/assets/stylesheets/admin.scss"
+
+      #check images
+      assert_directory "app/assets/images/admin"
     end
 
   end
