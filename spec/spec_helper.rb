c6934975cb24dbe077ae2ef8fefdaf32dcc87b7e
Oleg Bovykin mbpr <oleg.bovykin@gmail.com>
2012-07-27 19:11:35 +0400

    added draft scaffold generator

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,5 +4,6 @@ require 'active_record'
 require 'rspec'
 require 'generator_spec/test_case'
 require 'generators/redde/layout/layout_generator'
+require 'generators/redde/scaffold/scaffold_generator'
 Dir[Pathname.new(File.expand_path('../', __FILE__)).join('support/**/*.rb')].each {|f| require f}
 
