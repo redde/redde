80121471f020eb663385c81814715ecdc06bb91e
Oleg Bovykin mbpr <oleg.bovykin@gmail.com>
2012-09-04 20:35:12 +0400

    added mysql install scripts

--- lib/generators/redde/deploy/templates/recipes/database.rb
+++ lib/generators/redde/deploy/templates/recipes/database.rb
@@ -8,13 +8,46 @@ namespace :database do
   task :install, roles: :db do
     #run "sudo add-apt-repository ppa:pitti/postgresql"
     #run "sudo apt-get -y update"
+    begin
+      put %Q{
+        Name: mysql-server/root_password
+        Template: mysql-server/root_password
+        Value: 
+        Owners: mysql-server-5.1
+        Flags: seen
+
+        Name: mysql-server/root_password_again
+        Template: mysql-server/root_password_again
+        Value: 
+        Owners: mysql-server-5.1
+        Flags: seen
+
+        Name: mysql-server/root_password
+        Template: mysql-server/root_password
+        Value: 
+        Owners: mysql-server-5.0
+        Flags: seen
+
+        Name: mysql-server/root_password_again
+        Template: mysql-server/root_password_again
+        Value: 
+        Owners: mysql-server-5.0
+        Flags: seen
+      }, "non-interactive.txt"
+      sudo "DEBIAN_FRONTEND=noninteractive DEBCONF_DB_FALLBACK=Pipe apt-get -qq -y install mysql-server < non-interactive.txt"
+    rescue
+      raise
+    ensure
+      sudo "rm non-interactive.txt"
+    end
+
     run "sudo apt-get -y install libmysql-ruby libmysqlclient-dev"
   end
   after "deploy:install", "database:install"
 
   desc "Create a database for this application."
   task :create_database, roles: :db, only: {primary: true} do
-    run "sudo mysql -u #{database_user} -h #{database_host} -e 'create database #{database_database}'"
+    run "sudo mysql -u #{database_user} -h #{database_host} -e 'create database #{database_database} IF NOT EXISTS'"
   end
   after "deploy:setup", "database:create_database"
 
