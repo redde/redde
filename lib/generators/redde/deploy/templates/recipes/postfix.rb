0541940c17e66b0bcc6f404428e62bdb04fcddb8
Oleg Bovykin mbpr <oleg.bovykin@gmail.com>
2012-09-17 16:30:02 +0400

    fixed template_sudo

--- lib/generators/redde/deploy/templates/recipes/postfix.rb
+++ lib/generators/redde/deploy/templates/recipes/postfix.rb
@@ -1,13 +1,21 @@
 namespace :postfix do
+  def put_sudo(data, to)
+    filename = File.basename(to)
+    to_directory = File.dirname(to)
+    put data, "/tmp/#{filename}"
+    run "#{sudo} mv /tmp/#{filename} #{to_directory}"
+  end
+
   def template_sudo(from, to)
     erb = File.read(File.expand_path("../templates/#{from}", __FILE__))
     put_sudo ERB.new(erb).result(binding), to
   end
+
   desc "Install latest stable release of postfix"
   task :install, roles: :web do
     run "#{sudo} DEBIAN_FRONTEND=noninteractive apt-get -y install postfix"
     run "#{sudo} apt-get install -y dkim-filter"
-    run "#{sudo} mkdir /etc/postfix/dkim"
+    run "#{sudo} mkdir -p /etc/postfix/dkim"
     run "#{sudo} dkim-genkey -d #{fqdn} -s mail -r -D /etc/postfix/dkim/"
     run "#{sudo} mv /etc/postfix/dkim/mail.private /etc/postfix/dkim/mail"
     template_sudo "dkim-keys.conf.erb", "/etc/dkim-keys.conf"
