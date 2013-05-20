855275c0264aac5ea50e022773017b19ed2a37d5
Константин Горожанкин <konstantingorozankin@MacBook-Pro-Konstantin.local>
2013-05-20 13:00:16 +0400

    set tight authenticate

--- lib/generators/redde/layout/templates/controllers/application_controller.rb
+++ lib/generators/redde/layout/templates/controllers/application_controller.rb
@@ -1,15 +1,6 @@
-# coding: utf-8
+#coding: utf-8
 
 class Admin::ApplicationController < ActionController::Base
-  before_filter :authenticate_user!
-
-  layout :layout_by_resource
-  
-  def layout_by_resource
-    if devise_controller? && controller_name == "sessions"
-      'login'
-    else
-      "admin"
-    end
-  end
+  layout 'admin'
+  before_filter :authenticate_manager!
 end
