237aeb27f0a5961fe438e6a6afd126267bdfa5b8
Oleg Bovykin <oleg.bovykin@gmail.com>
2014-01-29 09:58:08 +0400

    Deleted deploy generator. Using chef instead

--- lib/generators/redde/deploy/templates/recipes/imagemagick.rb
+++
@@ -1,8 +0,0 @@
-namespace :imagemagick do
-  desc "Install latest imagemagick"
-  task :install, roles: :web do
-    run "#{sudo} apt-get install -y imagemagick libmagickcore-dev libmagickwand-dev"
-  end
-  after "deploy:install", "imagemagick:install"
-
-end
