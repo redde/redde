cabacba020305eaccfcbe9b4ea03c04ba9beb4b2
Oleg Bovykin <oleg.bovykin@gmail.com>
2014-09-18 14:03:24 +0400

    Fixing travis

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@ ENV['RAILS_ENV'] ||= 'test'
 
 require File.expand_path("../dummy/config/environment.rb",  __FILE__)
 
+require 'redde'
 require 'rspec/rails'
 require 'factory_girl_rails'
 require 'generator_spec/test_case'
