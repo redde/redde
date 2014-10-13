6578918bb2f4b5e5500add584814152e26a0e621
Konstantin Gorozhankin <konstantin.gorozhankin@gmail.com>
2014-10-13 20:13:42 +0400

    Обновил вьюшку редактирования

--- app/form_builders/redde_form_builder.rb
+++ app/form_builders/redde_form_builder.rb
@@ -1,63 +1,68 @@
 class ReddeFormBuilder < ActionView::Helpers::FormBuilder
-  delegate :content_tag, :tag, :raw, :link_to, :t, :render, :params, to: :@template
+  delegate :render, :content_tag, :tag, :link_to, to: :@template
 
-  def error_messages
-    render 'validate', { f: self }
+  def redde_field(name, *args)
+    label(name)
+    case object.class.columns.find { |column| column.name == name.to_s }.type
+      when :text then redde_text_area(name, *args)
+      when :boolean then redde_check_box(name, *args)
+    else
+      redde_text_field(name, *args)
+    end
   end
 
-  def inline_check_box(name, *args)
-    content_tag :tr do
+  # TODO: разобоаться с options
+  def redde_select(name, *args)
+    # options = args.extract_options!
+    content_tag :tr, class: options[:wrapper_class] do
+      content_tag(:th, smart_label(name)) + content_tag(:td, select(name, *args))
+    end
+  end
+
+  def redde_check_box(name, *args)
+    options = args.extract_options!
+    content_tag :tr, class: options[:wrapper_class] do
       content_tag :th, colspan: 2 do
-        check_box(name, *args) + ' ' + label(name) + '?'
+         check_box(name, options) + " " + smart_label(name)
       end
     end
   end
 
-  def inline_text_field(name, *args)
-    inline_field name, text_field(name, *args)
-  end
-
-  def inline_email_field(name, *args)
-    inline_field name, email_field(name, *args)
-  end
-
-  def inline_text_area(name, *args)
-    content_tag :tr do
-      content_tag :td, smart_label(name) + text_area(name, *args), colspan: 2
+  def redde_text_field(name, *args)
+    options = args.extract_options!
+    content_tag :tr, class: options[:wrapper_class] do
+      content_tag(:th, smart_label(name)) + content_tag(:td, text_field(name, options))
     end
   end
 
-  def select_or_create(name, valuesarray)
-    inline_field name, hidden_field(name, class: 'select_or_create', data: {valuesarray: valuesarray, placeholder: 'выберите или введите новое значение'})
-  end
-
-  def redde_select(name, opts, *args)
-    inline_field name, select(name, opts, *args)
-  end
-
-  def inline_submit
-    content_tag :div, class: 'actions' do
-      submit('Сохранить') + submit('Применить')
+  def redde_text_area(name, *args)
+    options = args.extract_options!
+    content_tag :tr, class: options[:wrapper_class] do
+      content_tag :td, colspan: 2 do
+        smart_label(name) + tag(:br) + text_area(name, options)
+      end
     end
   end
 
-  private
+  def redde_submit
+    content_tag :div, submit('Сохранить') + " " + submit('Применить'), class: 'actions'
+  end
 
-  def inline_field(name, some_field)
-    content_tag :tr do
-      inline_label(name) + content_tag(:td, some_field)
+  def error_messages
+    if object.errors.full_messages.any?
+      render 'validate', { f: self }
     end
   end
 
-  def inline_label(name)
-    content_tag :th, smart_label(name)
+private
+
+  def smart_label name
+    required = object.class.validators_on(name).any? { |v| v.kind_of? ActiveModel::Validations::PresenceValidator }
+    label(name, nil, class: ["redde-form__label",  ("_required" if required)])
   end
 
-  def smart_label(name)
-    label(name, nil, class: ['',  ('_required' if required_field?(name))])
+  def objectify_options options
+    super.except(:wrapper_class)
   end
 
-  def required_field?(name)
-    object.class.validators_on(name).any? { |v| v.kind_of? ActiveModel::Validations::PresenceValidator }
-  end
 end
