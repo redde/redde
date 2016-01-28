a5c40f6af858daf343bc5202df58018b73b1c660
Konstantin Gorozhankin <konstantin.gorozhankin@gmail.com>
2016-01-28 13:56:27 +0300

    индексная страница

--- app/assets/javascripts/redde-admin/index.js
+++
@@ -1,8 +0,0 @@
-//= require jquery
-//= require jquery_ujs
-
-// Пока подключаем все подряд, потом сделать кастомизацию
-//= require ./sortable
-//= require ./sortTree
-//= require ./fileapi
-//= require ./redactor
