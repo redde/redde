3a45c99a0f09eb4a29a712724fb0101795489031
Konstantin Gorozhankin <konstantin.gorozhankin@gmail.com>
2016-01-31 16:48:37 +0300

    добавил в миграцию доп. поля

--- spec/dummy/db/migrate/20160105092423_create_articles.rb
+++ spec/dummy/db/migrate/20160105092423_create_articles.rb
@@ -2,6 +2,7 @@ class CreateArticles < ActiveRecord::Migration
   def change
     create_table :articles do |t|
       t.string :title
+      t.datetime :published_at
       t.text :body
       t.timestamps null: false
     end
--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -23,9 +23,10 @@ ActiveRecord::Schema.define(version: 20160125163849) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
+    t.datetime "published_at"
     t.text     "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
   create_table "categories", force: :cascade do |t|
@@ -62,8 +63,8 @@ ActiveRecord::Schema.define(version: 20160125163849) do
     t.integer  "position"
     t.string   "src"
     t.string   "token"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
   end
 
   add_index "redde_photos", ["imageable_id"], name: "index_redde_photos_on_imageable_id"
