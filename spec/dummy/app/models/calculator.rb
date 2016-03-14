33f88b023840e3982efa29f5bfa41498067ae127
Oleg Bovykin <oleg.bovykin@gmail.com>
2016-03-14 18:53:07 +0300

    Added raw to output of customized view

--- spec/dummy/app/models/calculator.rb
+++ spec/dummy/app/models/calculator.rb
@@ -1,7 +1,7 @@
 class Calculator < ActiveRecord::Base
   INDEX_COLUMNS = {
     name: '',
-    hop: ->(item) { ActionController::Base.helpers.link_to("На сайт (#{2 * 2})", Rails.application.routes.url_helpers.root_path(id: item.id)) }
+    hop: ->(item) { "<br/>" +ActionController::Base.helpers.link_to("На сайт (#{2 * 2})", Rails.application.routes.url_helpers.root_path(id: item.id)) }
   }
   FORM_COLUMNS = %w(name)
 end
